42a517431f72919be1f093831104e9642700e8d5
Orlando <opseua@gmail.com>
2026-09-24 17:24:27 -0300

    globalThis removido da Cloudflare apenas para teste

--- src/CLOUDFLARE/Server/server.js
+++ src/CLOUDFLARE/Server/server.js
@@ -14,7 +14,7 @@ await import('../../src/resources/apiV2.js');
 export default {
 
     async fetch(request) {
-        let { status, headers, body, } = await globalThis.serverHandle({ 'method': request.method, 'getBody': () => request.json(), });
+        let { status, headers, body, } = await serverHandle({ 'method': request.method, 'getBody': () => request.json(), });
         return new Response(body, { headers, status, });
     },
 
